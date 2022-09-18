b8ae97c5ebfb184593d12bd365008a470be28753
ayushchakra <achakraborty@olin.edu>
2022-09-17 23:45:18 -0400

    added obstacle avoidance code

--- setup.py
+++ setup.py
@@ -23,7 +23,8 @@ setup(
             'teleop = comprobo_warmup_project.teleop:main',
             'drive_square = comprobo_warmup_project.drive_square:main',
             'marker_test = comprobo_warmup_project.marker_test_node:main'
-            'wall_follower = comprobo_warmup_project.wall_follower:main'
+            'wall_follower = comprobo_warmup_project.wall_follower:main',
+            'obstacle_avoider = comprobo_warmup_project.obstacle_avoider:main',
         ],
     },
 )
