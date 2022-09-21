75238b5a4e4a9fe4a1ba8a940fd0ff9ade79ec8c
Krishna Suresh <krishna4132@gmail.com>
2022-09-21 11:25:56 -0400

    docs

--- comprobo_warmup_project/wall_follower.py
+++ comprobo_warmup_project/wall_follower.py
@@ -4,30 +4,52 @@ from geometry_msgs.msg import Twist, Vector3
 from sensor_msgs.msg import LaserScan
 import math
 
+# Constants
+KP_WALL = 0.7 # coeff to proportionally control the wall distance correction
+KP_THETA = 1.0 # coeff to proportionally control the rotational correction
+SPEED = 0.1 # (m/s) constant forward speed of the neato 
+WALL_DIST = 0.45 # (m) target distance from the wall
+THETA_1 = 315  # (deg) 45 degrees after the right side of the neato
+THETA_2 = 235  # (deg)45 degrees before the right side of the neato
 
-Kp_wall = 0.7
-Kp_theta = 1.0
-SPEED = 0.1
-WALL_DIST = 0.45
+
 class WallFollowerNode(Node):
     def __init__(self):
         super().__init__('wall_follower_node')
-        self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
-        self.subscriber = self.create_subscription(LaserScan, 'scan', self.on_scan, 10)
+        self.cmd_vel_pub = self.create_publisher(Twist, 'cmd_vel', 10)
+        self.scan_sub = self.create_subscription(
+            LaserScan, 'scan', self.on_scan, 10)
 
-    def on_scan(self, msg : LaserScan):
+    def on_scan(self, msg: LaserScan):
+        """ On recieving a new scan from the neato lidar, control the wheel speeds to orient and follow a wall to the right side of the neato 
+        args: 
+            msg : LaserScan - lidar scan data from the /scan topic 
+        """
+        # filter out non existent lidar data
         valid_data = [a for a in msg.ranges if a != 0.0]
-        if not valid_data or msg.ranges[315] == 0.0 or msg.ranges[235] == 0:
+        if not valid_data or msg.ranges[THETA_1] == 0.0 or msg.ranges[THETA_2] == 0.0:
             print("WALL NOT FOUND")
-            vels = Twist(linear=Vector3(x=0.0, y=0.0, z=0.0), angular=Vector3(x=0.0,y=0.0,z=0.0)),
-            self.publisher.publish(vels)
+            self.set_cmd_vel(0, 0)
             return
+        # find the minimum lidar data point to find the orthogonal distance
         dist_to_wall = min(valid_data)
-        theta_wall = (math.pi/4)-math.atan2(msg.ranges[315], msg.ranges[235])
-        rotation_speed = (WALL_DIST-dist_to_wall)*Kp_wall + theta_wall*Kp_theta
-        print(f"DIST: {dist_to_wall}, THETA: {theta_wall}, SPEED:{rotation_speed}")
-        vels = Twist(linear=Vector3(x=SPEED, y=0.0, z=0.0), angular=Vector3(x=0.0,y=0.0,z=rotation_speed))
-        self.publisher.publish(vels)
+        # calculate the angle between the neato orientation and the wall plane
+        theta_wall = (math.pi/4)-math.atan2(msg.ranges[THETA_1], msg.ranges[THETA_2])
+        # rotational speed to achieve the target distance and parallel orientation
+        rotation_speed = (WALL_DIST-dist_to_wall)*KP_WALL + theta_wall*KP_THETA
+        print(
+            f"DIST: {dist_to_wall}, THETA: {theta_wall}, SPEED:{rotation_speed}")
+        self.set_cmd_vel(SPEED, rotation_speed)
+
+    def set_cmd_vel(self, linear_vel: float, angular_vel: float):
+        """ Sets the velocities of the neato by publishing to the /cmd_vel topic
+        args: 
+            linear_vel : float - m/s forward speed of the neato, in the x direction of the neato frame
+            angular_vel : float - rad/s rotating counter clockwise 
+        """
+        vel = Twist(linear=Vector3(x=linear_vel, y=0.0, z=0.0),
+                    angular=Vector3(x=0.0, y=0.0, z=angular_vel))
+        self.cmd_vel_pub.publish(vel)
 
 
 def main(args=None):
--- comprobo_warmup_project/yolo_test.py
+++ comprobo_warmup_project/yolo_test.py
@@ -2,14 +2,20 @@ import torch
 import cv2
 from yolov5.utils.plots import Annotator, colors
 from yolov5.models.common import Detections
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5n - yolov5x6, custom
+
+# Load yolov5 model and pretrained weights
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+# Set up webcam image capture
 cap = cv2.VideoCapture(2)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-stride, names, pt = model.stride, model.names, model.pt
+names = model.names # Load list of names from model
 while(True):
+    # Get current frame from webcam
     ret, frame = cap.read()
+    # Process frame into list of detections
     results : Detections = model(frame)
+    # Parse and output detections by class and confidence
     for pred in results.pred:
         if pred.shape[0]:
             for *box, conf, cls in reversed(pred):  # xyxy, confidence, class
@@ -17,6 +23,7 @@ while(True):
                 print(label)
         else:
             print("no detections")
+    # Display bounding boxes around detected objects
     cv2.imshow('preview', results.render(True)[0])
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
